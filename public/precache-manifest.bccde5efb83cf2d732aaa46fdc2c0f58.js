d172d726efd7052e7cf9acacf6a716dba87060de
DankDiego <diegosulcamedina33@gmail.com>
2022-02-25 05:55:21 -0500

    Version 1.0 CalendarAPP

---
+++ public/precache-manifest.bccde5efb83cf2d732aaa46fdc2c0f58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b259e2c31a71ef1f220076fda99e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d03f730e401858c694b",
+    "url": "/static/css/2.c7de1cb5.chunk.css"
+  },
+  {
+    "revision": "f2ef18f12c94a73b2f19",
+    "url": "/static/css/main.5c16ee17.chunk.css"
+  },
+  {
+    "revision": "7d03f730e401858c694b",
+    "url": "/static/js/2.31d8fdb0.chunk.js"
+  },
+  {
+    "revision": "afb90854ca170d941f072bc07eb8c0b6",
+    "url": "/static/js/2.31d8fdb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ef18f12c94a73b2f19",
+    "url": "/static/js/main.28dca2d6.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
